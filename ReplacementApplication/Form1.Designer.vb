1c8f7b4ace5731d1a269b24f5d74f119a5430058
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2023-11-21 21:42:34 +0330

    first commit

---
+++ ReplacementApplication/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblPathApplication = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'lblPathApplication
+        '
+        Me.lblPathApplication.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPathApplication.Location = New System.Drawing.Point(15, 171)
+        Me.lblPathApplication.Name = "lblPathApplication"
+        Me.lblPathApplication.Size = New System.Drawing.Size(401, 23)
+        Me.lblPathApplication.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 151)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Application Path:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Replacement Path:"
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Location = New System.Drawing.Point(15, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(401, 23)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = """C:\iranfilmport-temporary-replacement\"""
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 85)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Zipped File:"
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Location = New System.Drawing.Point(15, 105)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(401, 23)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = """C:\iranfilmport-temporary-replacement\test.zip"""
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Location = New System.Drawing.Point(15, 216)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(401, 58)
+        Me.ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.ProgressBar.TabIndex = 6
+        '
+        'BackgroundWorker
+        '
+        Me.BackgroundWorker.WorkerReportsProgress = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(436, 294)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblPathApplication)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Replacing ..."
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblPathApplication As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ProgressBar As ProgressBar
+    Friend WithEvents BackgroundWorker As System.ComponentModel.BackgroundWorker
+End Class
---
+++ ReplacementApplication/Form1.vb
@@ -0,0 +1,157 @@
+﻿Imports System.Net
+Imports System.IO.Compression
+Imports System.IO
+Imports System.Diagnostics
+Imports System.ComponentModel
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim path As String
+    Dim pathExtract As String = "C:\iranfilmport-temporary-replacement\"
+    Dim copyDestination As String
+    Dim filenameFromDataBase As String
+    Dim directoryUnzipPath As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        checkDownloadedFile()
+        '''''''''''''''''''''''''''''''''
+        Dim pr As New Process
+        For Each Process In pr.GetProcesses(My.Computer.Name)
+            If Process.ProcessName = "IFP" Then
+                Process.Kill()
+                Exit For
+            End If
+        Next
+        '''''''''''''''''''''''''''''''''
+        Dim _uri As New Uri(GetLink())
+        filenameFromDataBase = _uri.Segments(_uri.Segments.Length - 1)
+        directoryUnzipPath = filenameFromDataBase.Split(".")(0)
+        '''''''''''''''''''''''''''''''''
+        path = "C:\iranfilmport-temporary-replacement\" + filenameFromDataBase
+        copyDestination = readExePath()
+        UnZip()
+    End Sub
+
+    Private Sub checkDownloadedFile()
+        Dim di As New IO.DirectoryInfo(pathExtract)
+        If Not di.Exists Then
+            MessageBox.Show("The file is not downloaded yet!", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End
+        End If
+    End Sub
+
+    Dim sqlconn As New SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings("iranfilmportConnectionString").ConnectionString)
+    Public Function GetLink() As String
+        Try
+            If sqlconn.State = ConnectionState.Open Then sqlconn.Close()
+            sqlconn.Open()
+            Dim sqlcom As New SqlCommand("select [winapp] from tbl_setting", sqlconn)
+            Return sqlcom.ExecuteScalar
+            sqlconn.Close()
+        Catch ex As Exception
+        Finally
+            sqlconn.Close()
+        End Try
+    End Function
+
+    Function readExePath() As String
+
+        Dim line As String
+        Dim FilePath As String = "C:\iranfilmport-temporary-replacement\" & "exePath.txt"
+        ' Create new StreamReader instance with Using block.
+        Using reader As StreamReader = New StreamReader(FilePath)
+            ' Read one line from file
+            line = reader.ReadLine
+        End Using
+        lblPathApplication.Text = line
+        Return line
+
+    End Function
+
+    Private Sub UnZip()
+        Call RunProcessing()
+    End Sub
+
+    'Private Function setLoading(sourcepath As String) As Integer
+    '    Return IO.Directory.GetFiles(sourcepath, "*.*", SearchOption.AllDirectories).Length
+    'End Function
+
+    Sub RunProcessing()
+        BackgroundWorker.WorkerSupportsCancellation = True
+        BackgroundWorker.RunWorkerAsync()
+        If BackgroundWorker.IsBusy Then
+            BackgroundWorker.CancelAsync()
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker.ProgressChanged
+        ProgressBar.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub BackgroundWorker_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker.DoWork
+        Threading.Thread.Sleep(2000)
+        Try
+            'unziping ....
+            Dim zip As ZipArchive
+            Try
+                zip = ZipFile.OpenRead(path)
+            Catch ex As Exception
+                Windows.Forms.MessageBox.Show("Error : " & ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+            Finally
+                zip.Dispose()
+            End Try
+            Try
+                ZipFile.ExtractToDirectory(path, pathExtract)
+            Catch ex As Exception
+                Windows.Forms.MessageBox.Show("Error : " & ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+            Finally
+            End Try
+            'Get Directories and Files
+            Dim sourcepath As String = pathExtract + directoryUnzipPath
+            'setLoading(sourcepath)
+            Dim DestPath As String = copyDestination
+            'Delete the original files and directories
+            For Each file As IO.FileInfo In New IO.DirectoryInfo(DestPath).GetFiles
+                System.IO.File.Delete(DestPath & "\" & file.Name)
+            Next
+            For Each directory As IO.DirectoryInfo In New IO.DirectoryInfo(DestPath).GetDirectories
+                If directory.Name.ToLower <> "replacement" Then
+                    For Each file As IO.FileInfo In New IO.DirectoryInfo(DestPath & "\" & directory.Name).GetFiles
+                        System.IO.File.Delete(DestPath & "\" & directory.Name & "\" & file.Name)
+                    Next
+                    System.IO.Directory.Delete(DestPath & "\" & directory.Name)
+                End If
+            Next
+            'Copying
+            For Each _directory As IO.DirectoryInfo In New DirectoryInfo(sourcepath).GetDirectories
+                If _directory.Name.ToLower <> "replacement" Then
+                    IO.Directory.CreateDirectory(DestPath & "\" & _directory.Name)
+                    For Each file As IO.FileInfo In _directory.GetFiles
+                        file.CopyTo(DestPath & "\" & _directory.Name & "\" & file.Name)
+                    Next
+                End If
+            Next
+            For Each file As IO.FileInfo In New IO.DirectoryInfo(sourcepath).GetFiles
+                file.CopyTo(DestPath & "\" & file.Name)
+            Next
+            'Delete the temporary files and directories
+            For Each d In Directory.GetDirectories(pathExtract)
+                Directory.Delete(d, True)
+            Next
+            For Each f In Directory.GetFiles(pathExtract) 'Finish removing also the files in the root folder
+                File.Delete(f)
+            Next
+            System.IO.Directory.Delete(pathExtract, True) 'delete the main folder
+            'close automatically application
+            'System.Threading.Thread.Sleep(5000)
+            Process.Start(copyDestination & "/IFP.exe")
+            End
+
+        Catch ex As Exception
+            Windows.Forms.MessageBox.Show("Error : " & vbCrLf & "There is a problem with updating, you'll be closed automatically" & vbCrLf & ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+            End
+        End Try
+    End Sub
+
+End Class
